a50ce0fcc8989f6e08a0e51fff5a60f79a15ec85
NZY1337 <mandreicosmin@yahoo.com>
2021-11-02 16:03:27 +0200

    quill js theme changed  from bubble -> snow

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,24 +1,24 @@
-import React, { Component, createRef } from "react";
+import React, { Component, createRef } from 'react'
 
-import { withFirebase } from "../Firebase";
+import { withFirebase } from '../Firebase'
 
 // material-ui...
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import CardBlog from "./CardBlog";
-import HeaderContainer from "./HeaderContainer";
-import { SnackBarContext } from "../../utils/SnackBarContext";
-import Typography from "@material-ui/core/Typography";
-import LinearProgress from "@material-ui/core/LinearProgress";
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import CardBlog from './CardBlog'
+import HeaderContainer from './HeaderContainer'
+import { SnackBarContext } from '../../utils/SnackBarContext'
+import Typography from '@material-ui/core/Typography'
+import LinearProgress from '@material-ui/core/LinearProgress'
 
 class Blogs extends Component {
-  static contextType = SnackBarContext;
+  static contextType = SnackBarContext
 
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.pathname = this.props.location.pathname;
-    this.definedCategory = this.props.location.categ;
+    this.pathname = this.props.location.pathname
+    this.definedCategory = this.props.location.categ
 
     this.state = {
       latestDoc: null,
@@ -29,162 +29,186 @@ class Blogs extends Component {
       uniquePostId: null,
       uniqueStorageIdCallback: null,
       setCateg: null,
-      lastKey: "",
+      lastKey: '',
       nodeLength: null,
       showSpinner: false,
-    };
+    }
   }
 
   handleClick = (event, uniquePostId) => {
-    const post = this.state.posts.filter((post) => post.postId === uniquePostId);
+    const post = this.state.posts.filter((post) => post.postId === uniquePostId)
 
     this.setState({
       anchorEl: event.currentTarget,
       uniquePostId: uniquePostId,
       uniqueStorageIdCallback: post[0].uniqueStorageId,
-    });
-  };
+    })
+  }
 
   handleClose = () => {
     this.setState({
       anchorEl: null,
       uniquePostId: null,
-    });
-  };
+    })
+  }
 
   getRefLength = () => {
-    const ref = this.props.firebase.db.ref(`${this.pathname}`);
+    const ref = this.props.firebase.db.ref(`${this.pathname}`)
 
-    let self = this;
-    ref.once("value").then(function (snapshot) {
+    let self = this
+    ref.once('value').then(function (snapshot) {
       self.setState({
         nodeLength: snapshot.numChildren(),
-      });
-    });
-  };
+      })
+    })
+  }
 
   fetchFirstBatch = () => {
-    const firstBatchRef = this.props.firebase.db.ref(`${this.pathname}`).orderByKey().limitToFirst(4);
+    const firstBatchRef = this.props.firebase.db
+      .ref(`${this.pathname}`)
+      .orderByKey()
+      .limitToFirst(4)
 
     try {
-      firstBatchRef.on("child_added", (snapshot) => {
+      firstBatchRef.on('child_added', (snapshot) => {
         if (snapshot.val() !== null) {
-          let post = { ...snapshot.val(), postId: snapshot.key };
+          let post = { ...snapshot.val(), postId: snapshot.key }
 
           this.setState((prevState) => {
             return {
               ...prevState,
               posts: [...prevState.posts, post],
               lastKey: snapshot.key,
-            };
-          });
+            }
+          })
         }
-      });
+      })
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   fetchNext5Batches = () => {
     const next5BatchRef = this.props.firebase.db
       .ref(`${this.pathname}`)
       .orderByKey()
       .startAfter(this.state.lastKey)
-      .limitToFirst(4);
+      .limitToFirst(4)
 
     try {
-      next5BatchRef.on("child_added", (snapshot) => {
+      next5BatchRef.on('child_added', (snapshot) => {
         if (snapshot.val() !== null) {
-          let post = { ...snapshot.val(), postId: snapshot.key };
-          console.log("loading...");
+          let post = { ...snapshot.val(), postId: snapshot.key }
+          console.log('loading...')
           this.setState(
             (prevState) => {
               return {
                 ...prevState,
                 posts: [...prevState.posts, post],
                 lastKey: snapshot.key,
-              };
+              }
             },
             () => {
-              // we make sure that the state is updated and finishes the executions - due to asyncronicity
+              // we make sure that the state is updated and finishes the execution - due to asyncronicity
               if (this.state.posts.length === this.state.nodeLength) {
-                window.removeEventListener("scroll", this.onScrollFetchPosts, false);
-                this.setState({ showSpinner: true });
+                window.removeEventListener(
+                  'scroll',
+                  this.onScrollFetchPosts,
+                  false,
+                )
+                this.setState({ showSpinner: true })
               }
-            }
-          );
+            },
+          )
         }
-      });
+      })
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   fetchUser() {
     this.props.firebase.auth.onAuthStateChanged((user) => {
       if (user) {
-        this.setState({ user: user.uid });
+        this.setState({ user: user.uid })
       }
-    });
+    })
   }
 
   onScrollFetchPosts = () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-      this.fetchNext5Batches();
+      this.fetchNext5Batches()
+      console.log('fetched...')
     }
-  };
+  }
 
   handleDeletePost = ({ postType }, uniquePostId, uniqueStorageId) => {
-    const { storage, doRemoveItemsFromStorage } = this.props.firebase;
-    const postRefDB = this.props.firebase.db.ref(postType);
+    const { storage, doRemoveItemsFromStorage } = this.props.firebase
+    const postRefDB = this.props.firebase.db.ref(postType)
 
-    const confirm = window.confirm("are you sure you want to delete this?");
+    const confirm = window.confirm('are you sure you want to delete this?')
 
     if (confirm) {
-      const deletededPosts = this.state.posts.filter((post) => post.postId !== uniquePostId);
-      this.setState({ posts: deletededPosts });
+      const deletededPosts = this.state.posts.filter(
+        (post) => post.postId !== uniquePostId,
+      )
+      this.setState({ posts: deletededPosts })
 
       postRefDB
         .child(`${uniquePostId}`)
         .remove()
         .then(() => {
-          console.log(`${postType} deleted successfully`);
+          console.log(`${postType} deleted successfully`)
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
 
-      doRemoveItemsFromStorage(`/${postType}/${uniqueStorageId}/images/cover`, "Cover Photo");
+      doRemoveItemsFromStorage(
+        `/${postType}/${uniqueStorageId}/images/cover`,
+        'Cover Photo',
+      )
 
-      doRemoveItemsFromStorage(`/${postType}/${uniqueStorageId}/images/content/`, "Content Photos");
+      doRemoveItemsFromStorage(
+        `/${postType}/${uniqueStorageId}/images/content/`,
+        'Content Photos',
+      )
     }
 
-    this.handleClose();
-  };
+    this.handleClose()
+  }
 
   componentDidMount() {
-    window.scrollTo(0, 0);
-    this.getRefLength();
-    this.fetchFirstBatch();
-    this.fetchUser();
-    window.addEventListener("scroll", this.onScrollFetchPosts);
+    window.scrollTo(0, 0)
+    this.getRefLength()
+    this.fetchFirstBatch()
+    this.fetchUser()
 
-    this.snackBar = this.context;
+    window.addEventListener('scroll', this.onScrollFetchPosts)
 
-    console.log(window.innerHeight + window.scrollY, document.body.offsetHeight);
+    this.snackBar = this.context
+
+    //  console.log(window.innerHeight + window.scrollY, document.body.offsetHeight)
   }
 
   componentWillUnmount() {
-    window.removeEventListener("scroll", this.onScrollFetchPosts);
+    window.removeEventListener('scroll', this.onScrollFetchPosts)
   }
 
   render() {
-    const { anchorEl, uniquePostId, posts, user, filteredPosts, uniqueStorageIdCallback } = this.state;
+    const {
+      anchorEl,
+      uniquePostId,
+      posts,
+      user,
+      filteredPosts,
+      uniqueStorageIdCallback,
+    } = this.state
 
-    const open = Boolean(anchorEl);
+    const open = Boolean(anchorEl)
     const cover =
-      "https://images.pexels.com/photos/2029670/pexels-photo-2029670.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
+      'https://images.pexels.com/photos/2029670/pexels-photo-2029670.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
 
     const renderPosts = () => {
-      const decideWhatToRender = filteredPosts ? filteredPosts : posts;
+      const decideWhatToRender = filteredPosts ? filteredPosts : posts
 
       return (
         decideWhatToRender &&
@@ -203,10 +227,10 @@ class Blogs extends Component {
               handleDeletePost={this.handleDeletePost}
               uniqueStorageIdCallback={uniqueStorageIdCallback}
             />
-          );
+          )
         })
-      );
-    };
+      )
+    }
 
     return (
       <>
@@ -216,7 +240,10 @@ class Blogs extends Component {
           //   height="50vh"
           description="The BrandNu Design and Hip Hop Architecture Camp founder sits in his remixed version of an Eames lounge chair and ottoman outside the State Capitol in Madison, Wisconsin. Photography by Hedi Lamar Photography."
         />
-        <Container maxWidth="lg" style={{ marginTop: "7rem", marginBottom: "7rem" }}>
+        <Container
+          maxWidth="lg"
+          style={{ marginTop: '7rem', marginBottom: '7rem' }}
+        >
           <Grid spacing={2} container>
             {renderPosts()}
           </Grid>
@@ -227,15 +254,11 @@ class Blogs extends Component {
                 variant="p"
                 component="p"
                 style={{
-                  fontWeight: "bold",
-                  fontStyle: "italic",
-                  display: "block",
-                  marginTop: "1rem",
-                  textAlign: "center",
-                  background: "-webkit-linear-gradient(45deg, #09009f, #00ff95 80%)",
-                  backgroundClip: "border-box",
-                  webkitBackgroundClip: "text",
-                  webkitTextFillColor: "transparent",
+                  fontWeight: 'bold',
+                  fontStyle: 'italic',
+                  display: 'block',
+                  marginTop: '1rem',
+                  textAlign: 'center',
                 }}
               >
                 No More Posts to be loaded ...
@@ -245,8 +268,8 @@ class Blogs extends Component {
           </Grid>
         </Container>
       </>
-    );
+    )
   }
 }
 
-export default withFirebase(Blogs);
+export default withFirebase(Blogs)
--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,138 +1,146 @@
-import React from "react";
+import React from 'react'
 
 // uuid generates uniq keys for firebase-storage to get a ref for the uploaded files
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid'
 
 // quill
-import ReactQuill, { Quill } from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import "react-quill/dist/quill.bubble.css";
-import { ImageResize } from "quill-image-resize-module";
-import Paper from "@material-ui/core/Paper";
+import ReactQuill, { Quill } from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import 'react-quill/dist/quill.bubble.css'
+import { ImageResize } from 'quill-image-resize-module'
+import Paper from '@material-ui/core/Paper'
 
 // others...
-import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
-import { withFirebase } from "../Firebase";
-import Grid from "@material-ui/core/Grid";
-import HeaderContainer from "../Blog/HeaderContainer";
-import { SnackBarContext } from "../../utils/SnackBarContext";
+import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button'
+import { withFirebase } from '../Firebase'
+import Grid from '@material-ui/core/Grid'
+import HeaderContainer from '../Blog/HeaderContainer'
+import { SnackBarContext } from '../../utils/SnackBarContext'
 
 // Editor Preview
-import EditorPreview from "./editorPreview";
-import editorModules from "./quillModules";
-Quill.register("modules/imageResize", ImageResize);
+import EditorPreview from './editorPreview'
+import editorModules from './quillModules'
+Quill.register('modules/imageResize', ImageResize)
 
 const url =
-  "https://images.pexels.com/photos/323645/pexels-photo-323645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+  'https://images.pexels.com/photos/323645/pexels-photo-323645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
 
 class Editor extends React.Component {
-  static contextType = SnackBarContext;
+  static contextType = SnackBarContext
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.postId = this.props.match.params.id;
+    this.postId = this.props.match.params.id
 
-    this.postType = this.props.match.params.type;
-    this.uniqueStorageId = uuidv4().slice(0, 6);
-    this.editorRef = React.createRef();
+    this.postType = this.props.match.params.type
+    this.uniqueStorageId = uuidv4().slice(0, 6)
+    this.editorRef = React.createRef()
 
     this.state = {
       imgUploaded: false,
       quillEditor: null,
       content: {
-        editorContent: "",
-        postType: "blog",
-        category: "design de produs",
-        description: "",
-        cover: "",
-        title: "",
-        video: "",
-        uniqueStorageId: "",
+        editorContent: '',
+        postType: 'blog',
+        category: 'design de produs',
+        description: '',
+        cover: '',
+        title: '',
+        video: '',
+        uniqueStorageId: '',
         postViews: 0,
-        createdAt: new Date().toISOString().split("T")[0],
+        createdAt: new Date().toISOString().split('T')[0],
       },
       user: null,
-    };
+    }
   }
 
   //! to STORAGE
   handleUploadCoverImage = async (file) => {
-    const content = { ...this.state.content };
-    content.cover = file;
-    this.setState({ content });
-  };
+    const content = { ...this.state.content }
+    content.cover = file
+    this.setState({ content })
+  }
 
   handleAddCoverToFB = async (file) => {
-    const { uniqueStorageId, postType } = this.state.content;
-    const { storage } = this.props.firebase;
-    const { handleOpen } = this.context;
+    const { uniqueStorageId, postType } = this.state.content
+    const { storage } = this.props.firebase
+    const { handleOpen } = this.context
 
     // this.uniqueStorageId - first time submitting the post cover
     // uniqueStorageId - when the post is edited - from state
 
-    const decideCoverLocation = this.postId ? uniqueStorageId : this.uniqueStorageId;
+    const decideCoverLocation = this.postId
+      ? uniqueStorageId
+      : this.uniqueStorageId
 
     const imgRef = this.postId
-      ? storage.ref(`${postType}/${decideCoverLocation}/images/cover/${file.name}`)
-      : storage.ref(`${postType}/${decideCoverLocation}/images`).child(`cover/${file.name}`);
+      ? storage.ref(
+          `${postType}/${decideCoverLocation}/images/cover/${file.name}`,
+        )
+      : storage
+          .ref(`${postType}/${decideCoverLocation}/images`)
+          .child(`cover/${file.name}`)
 
     try {
-      this.setState({ imgUploaded: true });
-      const imgState = await imgRef.put(file);
-      const downloadUrl = await imgState.ref.getDownloadURL();
-      const content = { ...this.state.content };
-      content.cover = downloadUrl;
-      handleOpen("success", "Article's Post Cover uploaded successfully!");
-      content.uniqueStorageId = content.uniqueStorageId ? content.uniqueStorageId : this.uniqueStorageId;
-      this.setState({ imgUploaded: false, content: content });
+      this.setState({ imgUploaded: true })
+      const imgState = await imgRef.put(file)
+      const downloadUrl = await imgState.ref.getDownloadURL()
+      const content = { ...this.state.content }
+      content.cover = downloadUrl
+      handleOpen('success', "Article's Post Cover uploaded successfully!")
+      content.uniqueStorageId = content.uniqueStorageId
+        ? content.uniqueStorageId
+        : this.uniqueStorageId
+      this.setState({ imgUploaded: false, content: content })
     } catch ({ message }) {
-      handleOpen("error", message);
+      handleOpen('error', message)
     }
-  };
+  }
 
   handleAddPost = async () => {
     //! unmount firebase events when component unmounts
 
-    const content = { ...this.state.content };
+    const content = { ...this.state.content }
 
-    if (content.postType === "blog") {
-      delete content.category;
+    if (content.postType === 'blog') {
+      delete content.category
     }
 
-    this.setState({ content });
+    this.setState({ content })
 
-    if (typeof this.state.content.cover === "object") {
+    if (typeof this.state.content.cover === 'object') {
       // because file accepts a blog image, not the url from DB (url === 'string')
-      await this.handleAddCoverToFB(this.state.content.cover);
+      await this.handleAddCoverToFB(this.state.content.cover)
     }
 
-    const { handleOpen } = this.context;
-    const postRef = this.props.firebase.db.ref(`${this.state.content.postType}`);
+    const { handleOpen } = this.context
+    const postRef = this.props.firebase.db.ref(`${this.state.content.postType}`)
 
     if (this.postId) {
       postRef
         .child(`${this.postId}`)
         .update(this.state.content)
         .then(() => {
-          handleOpen("success", "Post updated successfully!");
-          this.props.history.push(`/${this.state.content.postType}`);
+          handleOpen('success', 'Post updated successfully!')
+          this.props.history.push(`/${this.state.content.postType}`)
         })
         .catch(({ message }) => {
-          handleOpen("error", message);
-        });
+          handleOpen('error', message)
+        })
     } else {
       postRef
         .push(this.state.content)
         .then(() => {
-          handleOpen("success", "Post created successfully!");
-          this.props.history.push(`/${this.state.content.postType}`);
+          handleOpen('success', 'Post created successfully!')
+          this.props.history.push(`/${this.state.content.postType}`)
         })
         .catch(({ message }) => {
-          handleOpen("error", message);
-        });
+          handleOpen('error', message)
+        })
     }
-  };
+  }
 
   //! to STORAGE
   handleUploadContentEditorImage = async (file) => {
@@ -140,120 +148,134 @@ class Editor extends React.Component {
         when uploading content images for editor we asume that we already have generated our Unique ID;
         because first we upload cover(immediately generates the UNIQUE_ID) then content
     */
-    const { handleOpen } = this.context;
-    const { storage } = this.props.firebase;
-    const { uniqueStorageId, postType } = this.state.content;
-    const decideCoverLocation = this.postId ? uniqueStorageId : this.uniqueStorageId;
+    const { handleOpen } = this.context
+    const { storage } = this.props.firebase
+    const { uniqueStorageId, postType } = this.state.content
+    const decideCoverLocation = this.postId
+      ? uniqueStorageId
+      : this.uniqueStorageId
 
     const imgRef = this.postId
-      ? storage.ref(`${postType}/${decideCoverLocation}/images/content/${file.name}`)
-      : storage.ref(`${postType}/${decideCoverLocation}/images/content`).child(`${file.name}`);
+      ? storage.ref(
+          `${postType}/${decideCoverLocation}/images/content/${file.name}`,
+        )
+      : storage
+          .ref(`${postType}/${decideCoverLocation}/images/content`)
+          .child(`${file.name}`)
 
     try {
-      const imgState = await imgRef.put(file);
-      const downloadUrl = await imgState.ref.getDownloadURL();
-      handleOpen("success", "Article's Image uploaded successfully!");
+      const imgState = await imgRef.put(file)
+      const downloadUrl = await imgState.ref.getDownloadURL()
+      handleOpen('success', "Article's Image uploaded successfully!")
 
-      return downloadUrl;
+      return downloadUrl
     } catch ({ message }) {
-      handleOpen("error", message);
+      handleOpen('error', message)
     }
-  };
+  }
 
   fetchPost = () => {
-    const postRef = this.props.firebase.db.ref(`${this.postType}/${this.postId}`);
+    const postRef = this.props.firebase.db.ref(
+      `${this.postType}/${this.postId}`,
+    )
 
-    postRef.on("value", (snapshot) => {
+    postRef.on('value', (snapshot) => {
       if (snapshot.val()) {
-        let content = { ...snapshot.val() };
-        content.editorContent = content.editorContent ? content.editorContent : "";
+        let content = { ...snapshot.val() }
+        content.editorContent = content.editorContent
+          ? content.editorContent
+          : ''
 
         this.setState({
           content: content,
-        });
+        })
       }
-    });
-  };
+    })
+  }
 
   onHandlePostPreview = (e) => {
-    const content = { ...this.state.content };
+    const content = { ...this.state.content }
 
-    if (e.currentTarget.type === "file") {
-      content.cover = e.currentTarget.files[0];
-      this.handleUploadCoverImage(e.currentTarget.files[0]);
-    } else if (e.currentTarget.type === "video") {
-      content.video = e.currentTarget.files[0];
+    if (e.currentTarget.type === 'file') {
+      content.cover = e.currentTarget.files[0]
+      this.handleUploadCoverImage(e.currentTarget.files[0])
+    } else if (e.currentTarget.type === 'video') {
+      content.video = e.currentTarget.files[0]
     } else {
-      content[e.target.name] = e.target.value;
+      content[e.target.name] = e.target.value
     }
 
     this.setState({
       content,
-    });
-  };
+    })
+  }
 
   createInput = () => {
-    const input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.setAttribute("accept", "image/*");
-    return input;
-  };
+    const input = document.createElement('input')
+    input.setAttribute('type', 'file')
+    input.setAttribute('accept', 'image/*')
+    return input
+  }
 
   quillImageCallback = async () => {
-    const input = this.createInput();
-    input.click();
+    const input = this.createInput()
+    input.click()
 
     input.onchange = async (e) => {
-      const file = e.target.files[0] || null;
+      const file = e.target.files[0] || null
 
       if (file) {
-        const range = this.state.quillEditor.editor.getSelection(true);
+        const range = this.state.quillEditor.editor.getSelection(true)
 
         this.setState({
           editor: this.state.quillEditor.editor.insertEmbed(
             range.index,
-            "image",
-            "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+            'image',
+            'https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif',
           ),
-        });
+        })
 
         this.setState({
           editor: this.state.quillEditor.editor.setSelection(range.index + 1),
-        });
+        })
 
-        const imgUrl = await this.handleUploadContentEditorImage(file);
+        const imgUrl = await this.handleUploadContentEditorImage(file)
 
         this.setState({
           editor: this.state.quillEditor.editor.deleteText(range.index, 1),
-        });
+        })
 
         this.setState({
-          editor: this.state.quillEditor.editor.insertEmbed(range.index, "image", imgUrl),
-        });
+          editor: this.state.quillEditor.editor.insertEmbed(
+            range.index,
+            'image',
+            imgUrl,
+          ),
+        })
       }
-    };
-  };
+    }
+  }
 
   handleChange = (html) => {
-    const content = { ...this.state.content };
-    content.editorContent = html;
-    this.setState({ content });
-  };
+    const content = { ...this.state.content }
+    content.editorContent = html
+    this.setState({ content })
+  }
 
   componentDidMount() {
     this.setState({
       quillEditor: this.editorRef.current,
-    });
+    })
 
-    if (this.postId) this.fetchPost();
+    if (this.postId) this.fetchPost()
 
     this.props.firebase.auth.onAuthStateChanged((user) => {
       if (user) {
         this.setState({
           user: user.uid,
-        });
+        })
       }
-    });
+    })
   }
 
   render() {
@@ -263,15 +285,23 @@ class Editor extends React.Component {
       content: {
         cover: { name: coverImgName },
       },
-    } = this.state;
-    const { title, description, cover, editorContent } = this.state.content;
-    const Quill_JS = editorModules.bind(this)();
-    const disabled = title === "" || description === "" || cover === "" || editorContent === "";
+    } = this.state
+    const { title, description, cover, editorContent } = this.state.content
+    const Quill_JS = editorModules.bind(this)()
+    const disabled =
+      title === '' || description === '' || cover === '' || editorContent === ''
 
     return (
       <>
-        <HeaderContainer cover={url} height="50vh" title={title ? title : "Create your story"} />
-        <Container maxWidth="lg" style={{ marginTop: "5rem", marginBottom: "5rem" }}>
+        <HeaderContainer
+          cover={url}
+          height="50vh"
+          title={title ? title : 'Create your story'}
+        />
+        <Container
+          maxWidth="lg"
+          style={{ marginTop: '5rem', marginBottom: '5rem' }}
+        >
           <EditorPreview
             onHandlePostPreview={this.onHandlePostPreview}
             imgUploaded={imgUploaded}
@@ -280,7 +310,12 @@ class Editor extends React.Component {
             postId={this.postId}
           />
 
-          <Grid container direction="row" justify="space-between" alignItems="flex-start">
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="flex-start"
+          >
             <Grid xs={12} item>
               <Paper elevation={3}>
                 <ReactQuill
@@ -288,8 +323,8 @@ class Editor extends React.Component {
                   value={content.editorContent}
                   ref={this.editorRef}
                   modules={Quill_JS.modules}
-                  style={{ padding: ".5rem", height: "100vh" }}
-                  theme="bubble" //bubble
+                  style={{ padding: '.5rem' }}
+                  theme="snow" //bubble
                   readOnly={false}
                   placeholder="Compose your story..."
                 />
@@ -301,15 +336,15 @@ class Editor extends React.Component {
             disabled={disabled}
             variant="contained"
             color="secondary"
-            style={{ marginTop: "12.5px" }}
+            style={{ marginTop: '12.5px' }}
             onClick={this.handleAddPost}
           >
             Publish Post
           </Button>
         </Container>
       </>
-    );
+    )
   }
 }
 
-export default withFirebase(Editor);
+export default withFirebase(Editor)
